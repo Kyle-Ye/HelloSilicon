0676cfb8d7b1d89c90bad54573b475b55aea04e1
Kyle <kyle201817146@gmail.com>
2022-05-16 18:09:06 +0800

    Add C03

---
+++ C03/Helloworld.s
@@ -0,0 +1,20 @@
+//
+//  Helloworld.s
+//  testasm
+//
+//  Created by Kyle on 2022/5/16.
+//
+.global _start
+.align 4
+
+_start:
+    mov x0, #1
+    adr x1, helloworld
+    mov x2, #13
+    mov x16, #4
+    svc #0x80
+
+    mov x0, #0
+    mov x16, #1
+    svc #0x80
+helloworld: .ascii "Hello World!\n"
---
+++ C03/testasm/testasm/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  testasm
+//
+//  Created by Kyle on 2022/5/16.
+//
+
+#import <Foundation/Foundation.h>
+
+extern void start(void);
+
+int main(int argc, const char * argv[]) {
+    start();
+    return 0;
+}
